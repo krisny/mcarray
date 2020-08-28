e1fef2cd124c71559bcef5f5b1a150a28f6e23ce
Kristian Nymoen <krisny@sauron.uio.no>
2020-08-28 16:31:48 +0200

    initial version of function. remove least typical examples across a single dimension

---
+++ mcarrayRemoveLeastNormal.m
@@ -0,0 +1,27 @@
+function dout = mcarrayRemoveLeastNormal(d, n, marker, dim)
+% remove the n "least typical" elements of a mcarray
+% only works for one marker in one dimension (Default uses marker 3 dimension 3)
+
+
+if nargin == 1
+    n = 5;
+    marker = 3;
+    dim = 3;
+end
+
+col = (marker-1)*3+dim;
+    
+for i = 1:length(d)
+    for j = 1:length(d)
+        distances(i,j) = sum(abs(d(i).data(:,col)-d(j).data(:,col)));
+    end
+end
+
+dsum = sortrows([1:length(d);sum(distances)]',-2);
+
+d(dsum(1:n,1)) = [];
+
+dout = d;
+    
+
+end
